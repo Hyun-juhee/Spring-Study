f34d7ea64fb23b1246f5ce557b9dfbf5c1872367
Hyun-juhee <juhe1998@naver.com>
2021-07-11 23:29:20 +0900

    feat(UserController, UserService, MemoryUserRepository) : 컴포넌트 스캔과 자동 의존관계 설정

--- user-management/src/main/java/com/example/usermanagement/controller/UserController.java
+++ user-management/src/main/java/com/example/usermanagement/controller/UserController.java
@@ -1,10 +1,12 @@
 package com.example.usermanagement.controller;
 
 import com.example.usermanagement.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class UserController {
     private final UserService userService;
 
+    @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
     }
