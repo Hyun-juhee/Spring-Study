5573143fdeaeef1977efe3865b12fcb012ac7745
Hyun-juhee <juhe1998@naver.com>
2021-07-17 22:11:51 +0900

    refactor(Order): [DI] DIP 원칙을 지키도록 수정. AppConfig로 부터 의존성을 주입받는다.

--- sale-management/src/test/java/spring/core/order/OrderServiceImplTest.java
+++ sale-management/src/test/java/spring/core/order/OrderServiceImplTest.java
@@ -1,21 +1,22 @@
 package spring.core.order;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import spring.core.AppConfig;
 import spring.core.member.Grade;
 import spring.core.member.Member;
 import spring.core.member.MemberService;
-import spring.core.member.MemberServiceImpl;
+import static org.assertj.core.api.Assertions.*;
 
 class OrderServiceImplTest {
-    MemberService memberService = new MemberServiceImpl();
-    OrderService orderService = new OrderServiceImpl();
+    AppConfig appConfig = new AppConfig();
+    MemberService memberService = appConfig.memberService();
+    OrderService orderService = appConfig.orderService();
     @Test
     void createOrder() {
         long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
         Order order = orderService.createOrder(memberId, "itemA", 10000);
-        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+        assertThat(order.getDiscountPrice()).isEqualTo(1000);
     }
 }
