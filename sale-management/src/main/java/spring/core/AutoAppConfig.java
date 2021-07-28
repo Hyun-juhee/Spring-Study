9c67d1db08a1f5390a214e2894f96934faf92e8b
Hyun-juhee <juhe1998@naver.com>
2021-07-29 00:38:29 +0900

    feat(AutoAppConfig): 의존관계 자동 주입

---
+++ sale-management/src/main/java/spring/core/AutoAppConfig.java
@@ -0,0 +1,15 @@
+package spring.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        basePackages = "spring.core",
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+public class AutoAppConfig {
+
+
+}
