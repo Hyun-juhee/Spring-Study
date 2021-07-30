d0cb242547e6cb05ea8b7eecaea61c42221f132d
Hyun-juhee <juhe1998@naver.com>
2021-07-30 10:40:57 +0900

    feat(HelloLombok): lombok 적용해보기

---
+++ sale-management/src/main/java/spring/core/HelloLombok.java
@@ -0,0 +1,20 @@
+package spring.core;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class HelloLombok {
+    private String name;
+    private int age;
+
+    public static void main(String[] args) {
+        HelloLombok helloLombok = new HelloLombok();
+        helloLombok.setName("juhi");
+        System.out.println(helloLombok.getName());
+        System.out.println("helloLombok = " + helloLombok);
+    }
+}
