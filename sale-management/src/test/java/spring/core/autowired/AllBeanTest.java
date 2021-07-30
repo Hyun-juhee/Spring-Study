bed7f95593021df2baf97d25ce16b72ebb49f7c8
Hyun-juhee <juhe1998@naver.com>
2021-07-30 12:15:47 +0900

    feat(AllBeanTest): 빈을 동적으로 선택할 때 다형성 이용하기

---
+++ sale-management/src/test/java/spring/core/autowired/AllBeanTest.java
@@ -0,0 +1,44 @@
+package spring.core.autowired;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import spring.core.AutoAppConfig;
+import spring.core.discount.DiscountPolicy;
+import spring.core.member.Grade;
+import spring.core.member.Member;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class AllBeanTest {
+    @Test
+    void findAllBean() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class, DiscountService.class);
+        DiscountService discountService = ac.getBean(DiscountService.class);
+        Member member = new Member(1L, "userA", Grade.VIP);
+
+        int discountPrice = discountService.discount(member, 10000, "fixDiscountPolicy");
+        assertThat(discountPrice).isEqualTo(1000);
+        int rateDiscountPrice = discountService.discount(member, 20000, "rateDiscountPolicy");
+        assertThat(rateDiscountPrice).isEqualTo(2000);
+    }
+
+    static class DiscountService {
+        private final Map<String, DiscountPolicy> policyMap;
+        private final List<DiscountPolicy> policies;
+
+        @Autowired
+        public DiscountService(Map<String, DiscountPolicy> policyMap, List<DiscountPolicy> policies) {
+            this.policyMap = policyMap;
+            this.policies = policies;
+        }
+
+        public int discount(Member member, int price, String discountCode) {
+            DiscountPolicy discountPolicy = policyMap.get(discountCode);
+            return discountPolicy.discount(member, price);
+        }
+    }
+}
