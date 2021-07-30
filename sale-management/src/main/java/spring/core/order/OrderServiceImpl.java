ed538127f3e21e4dde00eb512479d193e74a4845
Hyun-juhee <juhe1998@naver.com>
2021-07-30 11:32:55 +0900

    test(Qualifier): 같은 타입의 빈이 여러개 등록 되어 있을 때 구분 방법 1 - Qualifier

--- sale-management/src/main/java/spring/core/order/OrderServiceImpl.java
+++ sale-management/src/main/java/spring/core/order/OrderServiceImpl.java
@@ -1,17 +1,25 @@
 package spring.core.order;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import spring.core.discount.DiscountPolicy;
 import spring.core.member.Member;
 import spring.core.member.MemberRepository;
 
 @Component
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class  OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
+
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, @Qualifier("mainDiscountPolicy") DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
