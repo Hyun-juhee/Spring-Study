a4baa93c6ffc5df6caea1e08293511b05b08f39d
Hyun-juhee <juhe1998@naver.com>
2021-07-31 22:42:38 +0900

    refactor(scope): ObjectProvider로 prototype scope 활용하기

--- sale-management/src/test/java/spring/core/scope/SingletonWithPrototypeTest1.java
+++ sale-management/src/test/java/spring/core/scope/SingletonWithPrototypeTest1.java
@@ -1,7 +1,7 @@
 package spring.core.scope;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
@@ -34,21 +34,20 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
     static class ClientBean {
-        private final PrototypeBean prototypeBean; // 생성 시점에 주입 -> 계속 같은 인스턴스 사용
+        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
 
-        @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
+        public ClientBean(ObjectProvider<PrototypeBean> prototypeBeanProvider) {
+            this.prototypeBeanProvider = prototypeBeanProvider;
         }
 
         public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
             prototypeBean.addCount();
-            int count = prototypeBean.getCount();
-            return count;
+            return prototypeBean.getCount();
         }
     }
     @Scope("prototype")
